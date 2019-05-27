397a37d47387a65760da193ea748b486fa2e388d
Floris <Flomo123456789@gmail.com>
2019-05-27 09:35:10 +0100

    added early stateMachine still needs States to work with it

--- brain.py
+++ brain.py
@@ -1,6 +1,5 @@
 import motor as MotorControl
-
-
+import stateMachine/stateMachine as stateMachine
 
 print "right"
 MotorControl.turnRight(3)
---
+++ stateMachine/stateMachine.py
@@ -0,0 +1,16 @@
+import ../motor as MotorControl
+
+_states = {
+
+}
+
+_currentState = "somthing"
+
+def SetState(stateId):
+    if _states in _currentState.Keys():
+        print "return"
+        return
+    _currentState = _states[stateId]
+
+def AddState(stateId, State):
+    _states.Add(stateId, State)
