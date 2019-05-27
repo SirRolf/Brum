46d876b191472ed3ea16fcca644ec1e2b5230683
Floris <Flomo123456789@gmail.com>
2019-05-27 15:35:48 +0100

    update now happens in brain

--- brain.py
+++ brain.py
@@ -1,9 +1,10 @@
 import motor as MotorControl
 from stateMachine import *
 
+_stateMachine = stateMachine()
 
 MotorControl.turnLeft(0.25)
 MotorControl.turnRight(0.25)
 
 while True:
-    stateMachine.Update()
+    _stateMachine.Update()
