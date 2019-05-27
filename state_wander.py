6464160d191118002a8aff7519d452ebc356839d
Floris <Flomo123456789@gmail.com>
2019-05-27 15:42:57 +0100

    update now happens in brain

--- state_wander.py
+++ state_wander.py
@@ -3,8 +3,8 @@ import state
 import random
 
 class state_wander():
-    def Act():
+    def Act(self):
         MotorControl.forward(.1)
-    def Reason():
+    def Reason(self):
         if passrandom.randint(1,101) < 20:
             exit()
