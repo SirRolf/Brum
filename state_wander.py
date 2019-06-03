96aab461b544417f645a544e7b562e1645172aa7
Floris <Flomo123456789@gmail.com>
2019-06-03 12:49:29 +0100

    made him chose random actions. its realy short so you won't see a lot

--- state_wander.py
+++ state_wander.py
@@ -2,13 +2,17 @@ import motor as MotorControl
 from random import *
 from stateMachine import *
 
-
 class state_wander():
     def __init__(self, stateMachine):
         self._stateMachine = stateMachine
+        commands = [
+        MotorControl.forward(random() * .5),
+        MotorControl.backward(random() * .05),
+        MotorControl.motorsOff(random() * 1),
+        exit()
+        ]
     def Act(self):
-        MotorControl.forward(random() * .5)
+        choise(commands)
 
     def Reason(self):
-        if randint(0, 100) < 10:
-            self._stateMachine.SetState("wiggle")
+        pass
