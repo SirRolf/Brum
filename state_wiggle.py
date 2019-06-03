02bbe399c2d91aaf5b2f3e769298693d79912a5a
Floris <Flomo123456789@gmail.com>
2019-06-03 11:40:08 +0100

    added capital M to state wiggle

--- state_wiggle.py
+++ state_wiggle.py
@@ -5,7 +5,7 @@ class state_wiggle():
     def Act(self):
         MotorControl.turnLeft(0.25)
         MotorControl.turnRight(0.5)
-        motorControl.turnleft(0.25)
+        MotorControl.turnleft(0.25)
     def Reason(self):
         if randint(0, 100) < 50:
             exit()
