ab6ee62f9fae0cd2e4d8feea20537cf3db3ee0b4
Floris <Flomo123456789@gmail.com>
2019-06-03 13:37:01 +0100

    randomized the state wander even more, still needs specific random durations tho

--- state_wander.py
+++ state_wander.py
@@ -2,18 +2,17 @@ import motor as MotorControl
 import random
 from stateMachine import *
 
-commands = [
-MotorControl.forward(random.random() * 6),
-MotorControl.backward(random.random() * 1),
-MotorControl.wait(random.random() * 10),
-exit()
-]
-
 class state_wander():
     def __init__(self, stateMachine):
         self._stateMachine = stateMachine
+        commands = [
+        MotorControl.forward,
+        MotorControl.backward,
+        MotorControl.wait
+        ]
     def Act(self):
-        random.choice(commands)
+        random.choice(commands)(randomint(1,4))
 
     def Reason(self):
-        pass
+        if randint(0, 100) < 30:
+            exit()
