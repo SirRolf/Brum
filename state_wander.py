772e7bfac0b49f34b0c0472c3f5383da0d8bd5cb
Floris <Flomo123456789@gmail.com>
2019-06-03 13:10:48 +0100

    shouldn't run commands in constructor at state wander

--- state_wander.py
+++ state_wander.py
@@ -2,16 +2,18 @@ import motor as MotorControl
 import random
 from stateMachine import *
 
+commands = [
+MotorControl.forward(random.random() * 6),
+MotorControl.backward(random.random() * 1),
+MotorControl.wait(random.random() * 10),
+exit()
+]
+
 class state_wander():
     def __init__(self, stateMachine):
         self._stateMachine = stateMachine
-        commands = [
-        MotorControl.forward(random.random() * 6),
-        MotorControl.backward(random.random() * 1),
-        MotorControl.wait(random.random() * 10),
-        exit()
-        ]
     def Act(self):
-        pass
+        random.choice(commands)
+
     def Reason(self):
         pass
