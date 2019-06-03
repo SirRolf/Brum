182f67f8f92915688d795115bdf9c777ac5d7b8f
Floris <Flomo123456789@gmail.com>
2019-06-03 10:14:08 +0100

    made a class for init

--- brain.py
+++ brain.py
@@ -2,6 +2,7 @@ import motor as MotorControl
 from stateMachine import *
 
 _stateMachine = stateMachine()
+
 class init():
     while True:
         _stateMachine.Update()
--- state_wander.py
+++ state_wander.py
@@ -9,4 +9,4 @@ class state_wander():
         MotorControl.forward(.1)
     def Reason(self):
         if randint(0, 100) < 10:
-            _stateMachine.SetState(wiggle)
+            self._stateMachine.SetState(wiggle)
