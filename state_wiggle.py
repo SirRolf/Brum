cc2b6aa247e99e7a15c73b710b5069f5938283d3
Floris <Flomo123456789@gmail.com>
2019-06-03 11:38:31 +0100

    removed stop button and improved wiggle

--- state_wiggle.py
+++ state_wiggle.py
@@ -4,7 +4,8 @@ from random import randint
 class state_wiggle():
     def Act(self):
         MotorControl.turnLeft(0.25)
-        MotorControl.turnRight(0.25)
+        MotorControl.turnRight(0.5)
+        motorControl.turnleft(0.25)
     def Reason(self):
         if randint(0, 100) < 50:
             exit()
