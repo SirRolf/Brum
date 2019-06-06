a7343a308ba35e936b366417fe883a49c106b3bf
Sjors van der Kleij <24689@ma-web.nl>
2019-06-06 10:23:59 +0200

    sensor test

--- sensordetection.py
+++ sensordetection.py
@@ -1,4 +1,4 @@
-iimport RPi.GPIO as GPIO
+import RPi.GPIO as GPIO
 import time
 GPIO.setmode(GPIO.BCM)
 
@@ -10,19 +10,19 @@ print "Distance Measurement In Progress"
 GPIO.setup(TRIG,GPIO.OUT)
 GPIO.setup(ECHO,GPIO.IN)
 
-GPIO.output(TRIG, False)
-print "Waiting For Sensor To Settle"
+GPIO.output(TRIG,False)
+print "Settle Sensor For A Bit"
 time.sleep(2)
 
-GPIO.output(TRIG, True)
+GPIO.output(TRIG,True)
 time.sleep(0.00001)
-GPIO.output(TRIG, False)
+GPIO.output(TRIG,False)
 
 while GPIO.input(ECHO)==0:
-  pulse_start = time.time()
+    pulse_start = time.time()
 
 while GPIO.input(ECHO)==1:
-  pulse_end = time.time()
+    pulse_end = time.time()
 
 pulse_duration = pulse_end - pulse_start
 
@@ -30,6 +30,6 @@ distance = pulse_duration * 17150
 
 distance = round(distance, 2)
 
-print "Distance:",distance,"cm"
+print"Distance",distance,"cm"
 
 GPIO.cleanup()
