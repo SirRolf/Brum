6932e68bd6dbdd804265ecdeedff8c11d80b90ba
Floris <Flomo123456789@gmail.com>
2019-06-06 11:23:41 +0100

    removed function inside of sensor detection

--- sensordetection.py
+++ sensordetection.py
@@ -5,32 +5,28 @@ GPIO.setmode(GPIO.BCM)
 TRIG = 16
 ECHO = 20
 
-def sensordetect():
+GPIO.setup(TRIG,GPIO.OUT)
+GPIO.setup(ECHO,GPIO.IN)
 
-    GPIO.setup(TRIG,GPIO.OUT)
-    GPIO.setup(ECHO,GPIO.IN)
+GPIO.output(TRIG, False)
+time.sleep(2)
 
-    GPIO.output(TRIG, False)
-    time.sleep(2)
+GPIO.output(TRIG,True)
+time.sleep(0.00001)
+GPIO.output(TRIG,False)
 
-    GPIO.output(TRIG,True)
-    time.sleep(0.00001)
-    GPIO.output(TRIG,False)
+while GPIO.input(ECHO)==0:
+    pulse_start = time.time()
 
-    while GPIO.input(ECHO)==0:
-        pulse_start = time.time()
+while GPIO.input(ECHO)==1:
+    pulse_end = time.time()
 
-    while GPIO.input(ECHO)==1:
-        pulse_end = time.time()
+pulse_duration = pulse_end - pulse_start
 
-    pulse_duration = pulse_end - pulse_start
+distance = pulse_duration * 17150
 
-    distance = pulse_duration * 17150
+distance = round(distance, 2)
 
-    distance = round(distance, 2)
+print"Distance",distance,"cm"
 
-    print"Distance",distance,"cm"
-
-    GPIO.cleanup()
-
-sensordetect()
+GPIO.cleanup()
