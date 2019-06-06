e4b2ef9149890f2548116f89eaadcf9cd867f194
Sjors van der Kleij <24689@ma-web.nl>
2019-06-06 11:23:55 +0200

    sensor test 2

--- sensordetection.py
+++ sensordetection.py
@@ -5,31 +5,27 @@ GPIO.setmode(GPIO.BCM)
 TRIG = 16
 ECHO = 20
 
-print "Distance Measurement In Progress"
+def sensordetect():
 
-GPIO.setup(TRIG,GPIO.OUT)
-GPIO.setup(ECHO,GPIO.IN)
+    GPIO.setup(TRIG,GPIO.OUT)
+    GPIO.setup(ECHO,GPIO.IN)
 
-GPIO.output(TRIG,False)
-print "Settle Sensor For A Bit"
-time.sleep(2)
+    GPIO.output(TRIG,True)
+    time.sleep(0.00001)
+    GPIO.output(TRIG,False)
 
-GPIO.output(TRIG,True)
-time.sleep(0.00001)
-GPIO.output(TRIG,False)
+    while GPIO.input(ECHO)==0:
+        pulse_start = time.time()
 
-while GPIO.input(ECHO)==0:
-    pulse_start = time.time()
+    while GPIO.input(ECHO)==1:
+        pulse_end = time.time()
 
-while GPIO.input(ECHO)==1:
-    pulse_end = time.time()
+    pulse_duration = pulse_end - pulse_start
 
-pulse_duration = pulse_end - pulse_start
+    distance = pulse_duration * 17150
 
-distance = pulse_duration * 17150
+    distance = round(distance, 2)
 
-distance = round(distance, 2)
+    print"Distance",distance,"cm"
 
-print"Distance",distance,"cm"
-
-GPIO.cleanup()
+    GPIO.cleanup()
