eb81bfc884b6571741b0ca5a455a9447dfe90552
Floris <Flomo123456789@gmail.com>
2019-06-13 12:22:43 +0100

    added changelist to state-wander

--- state_wander.py
+++ state_wander.py
@@ -9,11 +9,11 @@ class state_wander():
         self._stateMachine = stateMachine
         # put your commands here
         self.commands = [
-        [MotorControl.forward, .2, .4],
+        [MotorControl.forward, .2, .4, [60,0,10,25,25]],
         [MotorControl.backward, .1, .2],
-        [MotorControl.wait, 2, 3],
-        [MotorControl.turnLeft, .2, .5],
-        [MotorControl.turnRight, .2, .5]
+        [MotorControl.wait, 2, 3, [60,0,25,10,10]],
+        [MotorControl.turnLeft, .2, .5, [60,0,10,25,25]],
+        [MotorControl.turnRight, .2, .5, [60,0,10,25,25]]
         ]
         self.commandChances = [
         60,
@@ -27,9 +27,14 @@ class state_wander():
         command = RandomList(self.commands, self.commandChances)
         # runs the script inside the commands array and gives it random times
         command[0](random.uniform(command[1],command[2]))
+        if command[3] is not None:
+            ChangeList(command[3])
 
     def Reason(self):
         # stops application if the distance of the sensor is lower then the value given
         if sensordetect() < 30:
             print("going to stay")
             self._stateMachine.SetState("stay")
+
+    def ChangeList(self, newChances):
+        self.commandChances = newChances
