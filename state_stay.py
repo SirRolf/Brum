a184fae0d1bc3cc87aac166e9ee52e8d160faf5c
Floris <Flomo123456789@gmail.com>
2019-06-14 08:46:46 +0100

    added the stay state

--- state_stay.py
+++ state_stay.py
@@ -30,6 +30,6 @@ class state_stay():
 
     def Reason(self):
         # going to wander again if this happens
-        if random.randint(1, 100) > 5:
+        if random.randint(1, 100) < 5:
             print("going to wander again")
             self._stateMachine.SetState("wander")
