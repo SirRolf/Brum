1356d7d03ceb55deed32fa1645f38c42a92cd4c7
Floris <Flomo123456789@gmail.com>
2019-06-17 15:04:05 +0100

    trying to make it so ctrl c makes the script close good

--- loop.py
+++ loop.py
@@ -5,14 +5,14 @@ import signal
 _stateMachine = stateMachine()
 
 class init():
+    def signal_handler(sig, frame):
+        print('You pressed Ctrl+C!')
+        MotorControl.motorsOff()
+        exit()
+        
     def run_program():
         while True:
             _stateMachine.Update()
             signal.signal(signal.SIGINT, signal_handler)
             print('Press Ctrl+C')
-
-    def signal_handler(sig, frame):
-        print('You pressed Ctrl+C!')
-        MotorControl.motorsOff()
-        exit()
     run_program()
