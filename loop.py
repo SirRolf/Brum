9cd78424d1be9501c2d87931b667729fd6e7e110
Floris <Flomo123456789@gmail.com>
2019-06-19 12:22:20 +0100

    final push

--- loop.py
+++ loop.py
@@ -1,18 +1,8 @@
 import motor as MotorControl
 from stateMachine import *
-import signal
 
 _stateMachine = stateMachine()
 
 class init():
-    def signal_handler(sig, frame):
-        print('You pressed Ctrl+C!')
-        MotorControl.motorsOff()
-        exit()
-
-    def run_program():
-        while True:
-            _stateMachine.Update()
-            signal.signal(signal.SIGINT, signal_handler)
-            print('Press Ctrl+C')
-    run_program()
+    while True:
+        _stateMachine.Update()
